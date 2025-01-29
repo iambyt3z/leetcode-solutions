d614b80e8ab7bfc66d4c3b29b7b7969d1ae856ba
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2025-01-28 21:18:04 -0800

    Time: 0 ms (100%), Space: 12.8 MB (93.68%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,45 +1,34 @@
 class Solution {
 public:
-    vector<int> res = {};
+    int find(int curr, vector<int> &parent) {
+        if(parent[curr] != curr)
+            parent[curr] = find(parent[curr], parent);
 
-    int findParent(int node, vector<int> &parent) {
-        int res = node;
-        while(res != parent[res]) {
-            res = parent[res];
-        }
-
-        return res;
+        return parent[curr];
     }
 
-    void join(vector<int> &edge, vector<int> &parent, vector<int> &rank) {
-        int p1 = findParent(edge[0], parent);
-        int p2 = findParent(edge[1], parent);
+    vector<int> findRedundantConnection(vector<vector<int>> &edges) {
+        int n = edges.size();
+        vector<int> parent;
+        for(int i=0; i<=n; i++)
+            parent.push_back(i);
 
-        if(p1 == p2) {
-            res = edge;
-            return ;
+        for(int i=0; i<n; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            int pu = find(u, parent);
+            int pv = find(v, parent);
+
+            if(pu == pv)
+                return {u, v};
+
+            if(pu < pv)
+                parent[pv] = pu;
+            else
+                parent[pu] = pv;
         }
 
-        if(rank[p2] > rank[p1]) {
-            rank[p2] += rank[p1];
-            parent[p1] = p2;
-        } else {
-            rank[p1] += rank[p2];
-            parent[p2] = p1;
-        }
-    }
-
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        vector<int> parent(edges.size() + 1);
-        iota(parent.begin(), parent.end(), 0);
-        vector<int> rank(edges.size() + 1, 1);
-
-        for(vector<int> edge: edges) {
-            join(edge, parent, rank);
-            
-            if(res.size()) break;
-        }
-
-        return res;
+        return {};
     }
 };
