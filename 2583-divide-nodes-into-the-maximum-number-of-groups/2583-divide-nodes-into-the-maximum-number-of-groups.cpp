cbc1b2d32c2a181446f545f6ef6573d1866d9ea1
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2025-01-29 18:09:22 -0800

    Time: 123 ms (86.55%), Space: 53.3 MB (63.03%) - LeetHub

--- 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.cpp
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -13,7 +13,6 @@ public:
                 bool markNext = isBipartite(next, 1 - curr_group, group, adj, nodes_covered);
                 if(!markNext) return false;
             }
-
             else if(group[next] == curr_group)
                 return false;
         }
@@ -21,69 +20,51 @@ public:
         return true;
     }
 
-    vector<vector<int>> calcMinDist(int n, vector<vector<int>> &adj) {
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-    
-        for(int i = 0; i < n; i++) {
-            dist[i][i] = 0;
-        }
-        
-        for(int u=0; u<n; u++) {
-            for(auto v: adj[u]) {
-                dist[u][v] = min(dist[u][v], 1);
-            }
-        }
-        
-        for(int k = 0; k < n; k++) {
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    if(dist[i][k] != INT_MAX && dist[k][j] != INT_MAX) {
-                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-                    }
-                }
-            }
-        }
-        
-        return dist;
-    }  
-
     int magnificentSets(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n, vector<int>());
-        int e = edges.size();
-
-        for(int i=0; i<e; i++) {
-            int u = edges[i][0] - 1;
-            int v = edges[i][1] - 1;
-
+        vector<vector<int>> adj(n);
+        for (auto& e : edges) {
+            int u = e[0] - 1, v = e[1] - 1;
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
 
         vector<int> group(n, -1);
-        vector<vector<int>> minDist = calcMinDist(n, adj);
         int res = 0;
 
-        for(int i=0; i<n; i++) {
-            if(group[i] != -1)
-                continue;
+        for (int i = 0; i < n; ++i) {
+            if (group[i] != -1) continue;
 
-            vector<int> nodes_covered;
-            bool isBip = isBipartite(i, 0, group, adj, nodes_covered);
-
-            if(!isBip)
+            vector<int> component;
+            if (!isBipartite(i, 0, group, adj, component)) {
                 return -1;
-
-            int ri = 0;
-            int len = nodes_covered.size();
-            for(int i=0; i<len-1; i++) {
-                for(int j=i+1; j<len; j++) {
-                    int u = nodes_covered[i];
-                    int v = nodes_covered[j];
-                    ri = max(ri, minDist[u][v]);
-                }
             }
 
-            res += (ri+1);
+            int maxDist = 0;
+            for (int u : component) {
+                vector<int> dist(n, -1);
+                queue<int> q;
+                dist[u] = 0;
+                q.push(u);
+                int current_max = 0;
+
+                while (!q.empty()) {
+                    int curr = q.front();
+                    q.pop();
+                    for (int v : adj[curr]) {
+                        if (dist[v] == -1) {
+                            dist[v] = dist[curr] + 1;
+                            if (dist[v] > current_max) {
+                                current_max = dist[v];
+                            }
+                            q.push(v);
+                        }
+                    }
+                }
+
+                maxDist = max(maxDist, current_max);
+            }
+
+            res += maxDist + 1;
         }
 
         return res;
