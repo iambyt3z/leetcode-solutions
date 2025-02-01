dcbf4dcfb034aa5a952b0b39d923d2ac2f42def4
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2025-02-01 13:53:24 -0800

    Time: 64 ms (20.53%), Space: 32.3 MB (19.14%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int dp[201][201];
+
+    int solve(int i, int j, int n, int m, vector<vector<int>>& matrix) {
+        // cout << i << " " << j << endl;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        int res = 1;
+
+        vector<int> dx = {1, -1, 0, 0};
+        vector<int> dy = {0, 0, 1, -1};
+
+        for(int d=0; d<4; d++) {
+            int ii = i + dx[d];
+            int jj = j + dy[d];
+
+            if( ii < 0 || ii >= n || jj < 0 || jj >= m || matrix[i][j] <= matrix[ii][jj] )
+                continue;
+
+            res = max(res, 1 + solve(ii, jj, n, m, matrix));
+        }
+
+        return dp[i][j] = res;
+    }
+
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int res = 0;
+
+        memset(dp, -1, sizeof(dp));
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                res = max(res, solve(i, j, n, m, matrix));
+            }
+        }
+
+        return res;
+    }
+};
