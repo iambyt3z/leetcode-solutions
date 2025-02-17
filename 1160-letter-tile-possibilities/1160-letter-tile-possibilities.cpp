9d313d23bb86a9d994afdfb3a372d2263bbc8b4d
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2025-02-16 17:44:09 -0800

    Time: 0 ms (100%), Space: 8.9 MB (62.93%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int fact(int n) {
+        if(n == 0 || n == 1)
+            return 1;
+        return n * fact(n-1);
+    }
+
+    int helper(string &s) {
+        vector<int> cnt(26, 0);
+        int len = s.length();
+        int res = fact(len);
+
+        for(char ch: s) {
+            int ind = (int) (ch - 'A');
+            cnt[ind]++;
+        }
+
+        for(int i=0; i<26; i++)
+            res /= fact(cnt[i]);
+
+        return res;
+    }
+
+    int numTilePossibilities(string tiles) {
+        unordered_map<string, int> umap;
+        int n = tiles.length();
+        int res = 0;
+
+        for(int state=0; state<pow(2, n); state++) {
+            string tmp = "";
+
+            for(int i=0; i<n; i++) {
+                int leftShift = n-1-i;
+                int bitmask = (1 << leftShift);
+                if(state & bitmask) tmp.push_back(tiles[i]);
+            }
+
+            sort(tmp.begin(), tmp.end());
+            if(tmp.length() == 0 || umap.count(tmp) != 0) continue;
+
+            // cout << "tmp = " << tmp << endl;
+
+            int ri = helper(tmp);
+            umap[tmp] = ri;
+            res += ri;
+        }
+
+        return res;
+    }
+};
