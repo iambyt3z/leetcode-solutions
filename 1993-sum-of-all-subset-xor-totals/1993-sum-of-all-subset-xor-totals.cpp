f76bafa1ef77a4a465e4dc56a511ee06e35e3455
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2025-04-04 20:30:35 -0700

    Time: 1 ms (47.63%), Space: 9.2 MB (42.82%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -5,19 +5,14 @@ public:
         int len = nums.size();
 
         for(int state = 0; state < pow(2, len); state++) {
-            // cout << "state = " << state << endl;
             int curr = 0;
 
             for(int i=0; i<len; i++) {
                 int bitmask = (1 << i);
-                if(bitmask & state) {
-                    curr ^= nums[len-1-i];
-                    // cout << "Added: " << nums[len-1-i] << endl;
-                }
+                curr ^= ((bitmask & state) != 0) * nums[len-1-i];
             }
 
             res += curr;
-            // cout << "curr = " << curr << endl;
         }
 
         return res;
