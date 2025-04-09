ce1857554a1dcd28a38169241d9ba08a07badcee
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2025-04-09 02:28:43 -0700

    Time: 0 ms (100%), Space: 76.9 MB (85.86%) - LeetHub

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -1,35 +1,9 @@
 class Solution {
 public:
-    int minNumberOperations(vector<int>& s) {
-        int n = s.size();
-        vector<int> left(n, 0), right(n, 0);
-        stack<int> leftS, rightS;
-        
-        for(int i=0; i<n; i++) {
-            while(leftS.size() && s[i] <= leftS.top())
-                leftS.pop();
-                
-            if(leftS.size()) left[i] = leftS.top();
-            else left[i] = 1;
-            
-            leftS.push(s[i]);
-        }
-        
-        for(int i=n-1; i>=0; i--) {
-            while(rightS.size() && s[i] < rightS.top())
-                rightS.pop();
-                
-            if(rightS.size()) right[i] = rightS.top();
-            else right[i] = 1;
-            
-            rightS.push(s[i]);
-        }
-        
-        long res = 0;
-        
-        for(int i=0; i<n; i++)
-            res += (long) min(s[i] - left[i], s[i] - right[i]);
-        
-        return res + 1;
+    int minNumberOperations(vector<int>& target) {
+        int count = target[0];
+        for (int i = 1; i < target.size(); i++)
+            count += max(target[i] - target[i - 1], 0);
+        return count;
     }
 };
