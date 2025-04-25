0fb610da77cd64a910c2f982e7f0744a18f1dbcf
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2025-04-24 20:14:31 -0700

    Time: 87 ms (17.49%), Space: 129.2 MB (6.56%) - LeetHub

---
+++ 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int m, int k) {
+        int n = nums.size();
+        long long prefix = 0, res = 0;
+        unordered_map<int, int> cnt;
+        cnt[0]++;
+
+        for(int i=0; i<n; i++) {
+            prefix += (nums[i] % m == k);
+            int a = prefix % m;
+            int b1 = a - k;
+            int b2 = a - k + m;
+
+            // cout << prefix << " ";
+            // cout << a << " " << b1 << " " << b2 << endl;
+
+            res += (b1 >= 0) * cnt[b1] + (b2 >= 0) * cnt[b2];
+            cnt[a]++;
+        }
+
+        return res;
+    }
+};
