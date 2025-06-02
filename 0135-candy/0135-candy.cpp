9e67f7966033e682f3ba09185e791576ddd50fe5
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2025-06-01 20:36:44 -0700

    Time: 0 ms (100%), Space: 22.8 MB (59.49%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> cadies(n, 1);
+
+        // for(auto ci: cadies) cout << ci << " ";
+        // cout << endl;
+
+        for(int i=1; i<n; i++)
+            if(ratings[i] > ratings[i-1] && cadies[i] <= cadies[i-1])
+                cadies[i] = 1 + cadies[i-1];
+        
+        // for(auto ci: cadies) cout << ci << " ";
+        // cout << endl;
+
+        for(int i=n-2; i>=0; i--)
+            if(ratings[i] > ratings[i+1] && cadies[i] <= cadies[i+1])
+                cadies[i] = 1 + cadies[i+1];
+        
+        // for(auto ci: cadies) cout << ci << " ";
+        // cout << endl;
+
+        return accumulate(cadies.begin(), cadies.end(), 0);
+    }
+};
