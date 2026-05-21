988cc6ed36ce0537a92b946b45d722e4fd74f5dd
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2026-05-20 22:53:02 -0700

    Time: 156 ms (60.83%), Space: 224.5 MB (9.16%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,79 +1,73 @@
+class TrieNode {
+    public:
+        bool endOfWord;
+        TrieNode* children[10];
+
+        TrieNode() {
+            endOfWord = false;
+            for(int i=0; i<10; i++)
+                children[i] = nullptr;
+        }        
+};
+
+class Trie {
+    private:
+        TrieNode* root;
+
+    public:
+        Trie() {
+            root = new TrieNode();
+        }
+
+        Trie(vector<string> &keys) {
+            root = new TrieNode();
+            for(string &key: keys)
+                this->insert(key);
+        }
+
+        void insert(string &key) {
+            TrieNode* curr = root;
+            for(char ch: key) {
+                int ind = ch - '0';
+                if(curr->children[ind] == nullptr) {
+                    curr->children[ind] = new TrieNode();
+                }
+
+                curr = curr->children[ind];
+            }
+
+            curr->endOfWord = true;
+        }
+
+        int getMaxLengthPrefix(string &key) {
+            int res = 0;
+            TrieNode* curr = root;
+            for(char ch: key) {
+                int ind = ch - '0';
+                curr = curr->children[ind];
+
+                if(curr == nullptr)
+                    break;
+
+                res++;
+            }
+
+            return res;
+        }
+};
+
 class Solution {
 public:
-    int leftBound(int l, int r, vector<string> &a1, char target, int digit) {
-        int res = -1;
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-
-            if(digit >= a1[mid].size()) {
-                l = mid+1;
-                continue;
-            }
-
-            char curr = a1[mid][digit];
-
-            if(curr < target) {
-                l = mid+1;
-            } else if (curr == target) {
-                r = mid-1;
-                res = mid;
-            } else {
-                r = mid-1;
-            }
-        }
-
-        return res;
-    }
-
-    int rightBound(int l, int r, vector<string> &a1, char target, int digit) {
-        int res = -1;
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-
-            if(digit >= a1[mid].size()) {
-                l = mid+1;
-                continue;
-            }
-
-            char curr = a1[mid][digit];
-
-            if(curr < target) {
-                l = mid+1;
-            } else if (curr == target) {
-                l = mid+1;
-                res = mid;
-            } else {
-                r = mid-1;
-            }
-        }
-
-        return res;
-    }
-
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         vector<string> a1, a2;
         for(int num: arr1) a1.push_back(to_string(num));
         for(int num: arr2) a2.push_back(to_string(num));
-
-        sort(a1.begin(), a1.end());
+        Trie* trie = new Trie(a1);
 
         int res = 0;
-        for(int i=0; i<a2.size(); i++) {
-            string a2i = a2[i];
-            int l=0, r=a1.size()-1;
-            int ri=0; int digit=0;
-
-            while(digit < a2i.size() && l != -1) {
-                int newL = leftBound(l, r, a1, a2i[digit], digit);
-                int newR = rightBound(l, r, a1, a2i[digit], digit);
-
-                ri += (newL != -1);
-                l = newL;
-                r = newR;
-                digit++;
-            }
-
-            res = max(res, ri);
+        for(string &num: a2) {
+            int ri = trie->getMaxLengthPrefix(num);
+            res = max(ri, res);
         }
 
         return res;
